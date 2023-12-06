0d653c8eb9125fc9446e1ad9696457f72e124a49
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2023-12-06 17:03:55 -0500

    Added description, installation, usage, contributing and test sections to generated README

--- index.js
+++ index.js
@@ -1,50 +1,67 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateREADME = ({ name, location, github, linkedin }) =>
-`# 09 Node.js Challenge: Professional README Generator
+const generateREADME = ({ title, description, installation, usage, contributing, tests }) =>
+`# ${title}
 
-## Your Task
+## Description
 
-When creating an open source project on GitHub, its important to have a high-quality README for the app. This should include what the app is for, how to use the app, how to install it, how to report issues, and how to make contributions&mdash;this last part increases the likelihood that other developers will contribute to the success of the project. 
+${description}
 
-You can quickly and easily create a README file by using a command-line application to generate one. This allows the project creator to devote more time to working on the project.
+## Table of Contents
 
-Your task is to create a command-line application that dynamically generates a professional README.md file from a user's input using the [Inquirer package](https://www.npmjs.com/package/inquirer/v/8.2.4). Review the [Professional README Guide](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide) as a reminder of everything that a high-quality, professional README should contain. 
+## Installation
 
-The application will be invoked by using the following command:`;
+${installation}
+
+## Usage
+
+${usage}
+
+## License
+
+## Contributing
+
+${contributing}
+
+## Tests
+
+${tests}
+
+## Questions
+`;
 
 inquirer
   .prompt([
     {
       type: 'input',
-      name: 'name',
-      message: 'What is your name?',
+      name: 'title',
+      message: 'What is your project\'s title?',
     },
     {
       type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
+      name: 'description',
+      message: 'Enter a project description:',
     },
     {
       type: 'input',
-      name: 'hobby',
-      message: 'What is your favorite hobby?',
+      name: 'installation',
+      message: 'Enter installation instructions:',
     },
     {
       type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
+      name: 'usage',
+      message: 'Enter usage information:',
     },
     {
       type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username',
+      name: 'contributing',
+      message: 'Enter contribution instructions:',
     },
     {
       type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
+      name: 'tests',
+      message: 'Enter test instructions:',
     },
   ])
   .then((answers) => {
